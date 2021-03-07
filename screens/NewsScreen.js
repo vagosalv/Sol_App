7b2501dbd539c7b8d5b0a0f5a5061a4a9e24e604
vagosalv <vagosalvgd@gmail.com>
2021-03-07 20:49:32 +0200

    fix NewsScreen

--- screens/NewsScreen.js
+++ screens/NewsScreen.js
@@ -1,50 +1,57 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, FlatList, TouchableOpacity, Button, Linking, } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, } from 'react-native';
+import RNUrlPreview from 'react-native-url-preview';
 
 const NewsScreen = ({navigation, route}) => {
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Διαστημικά νέα</Text>
-            <View style={styles.news}>
-                <Text onPress={() => Linking.openURL('https://www.nasa.gov/')}>Nasa Site</Text>
+            <View>
+                <ScrollView>
+                    <RNUrlPreview containerStyle={styles.news} imageProps={styles.image} text={"any text to be parsed , https://www.nasa.gov/press-release/nasa-spacex-invite-media-to-next-commercial-crew-launch/"}/>
+                    <RNUrlPreview containerStyle={styles.news} imageProps={styles.image} text={"any text to be parsed , https://www.nasa.gov/press-release/nasa-s-perseverance-drives-on-mars-terrain-for-first-time"}/>
+                    <RNUrlPreview containerStyle={styles.news} imageProps={styles.image} text={"any text to be parsed , https://spacenews.com/nasa-hikes-prices-for-commercial-iss-users/"}/>
+                    <RNUrlPreview containerStyle={styles.news} imageProps={styles.image} text={"any text to be parsed , https://spacenews.com/dod-space-agency-to-award-multiple-contracts-for-up-to-150-satellites/"}/>
+                    <RNUrlPreview containerStyle={styles.news} imageProps={styles.image} text={"any text to be parsed , https://spacenews.com/vega-c-a-new-generation-launcher/"}/>
+                </ScrollView>
+                
             </View>
             <View style={styles.buttons}>
-                <Button color='black'  title='Back' onPress={() => navigation.goBack()}></Button>
             </View>
         </View>
     );
 };
 
-
 export default NewsScreen;
 
 
-// h css 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
         marginVertical: 8,
+        padding: 10,
+        margin: 10,
     },
-
     title: {
-        flex: 0.05,
+        fontSize: 25,
+        padding: 5,
+        fontWeight: 'bold',
+        textDecorationLine: 'underline',
         textAlign: 'center',
-        fontSize: 22,
-        borderBottomColor: '#737373',
-        borderBottomWidth: 1,
     },
-
-    buttons: {
-        flexDirection: 'column',
-        marginHorizontal: 100,
-        marginVertical: 8,
-        color: 'red',
-    },
-
     news: {
-        flexDirection: 'column',
-        marginHorizontal: 165,
-        marginVertical: 8,
+        marginTop: 15,
+        backgroundColor: 'lightgrey',
+        borderRadius: 20,
+        width: '100%',
+        height: '25%',
+    },
+    titleImage: {
+        borderRadius: 20,
+    },
+    image: {
+       width: 0.5,
+       height: 0.5, 
+       borderRadius: 50,
     },
 });
